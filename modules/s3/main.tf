9efa35ed530e4dc3de6c584174274ff0489b5974
Deekshitha R <149041394+Rdeekshitha123@users.noreply.github.com>
2025-07-23 16:33:37 +0530

    added a bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,16 +4,12 @@ bucket="devon-credentials001"
 force_destroy=true
 }
 
-resource "aws_s3_bucket" "devon-credentials002"{
-bucket="devon-credentials002"
-
-force_destroy=true
-}
 resource "aws_s3_bucket" "devon-credentials006"{
 bucket="devon-credentials006"
 
 force_destroy=true
 }
+
 resource "aws_s3_bucket" "devon-credentials007"{
 bucket="devon-credentials007"
 
