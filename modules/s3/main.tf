ac47c28b4405315f2fc9a8d6f02e32f5575c13fd
Deekshitha R <149041394+Rdeekshitha123@users.noreply.github.com>
2025-08-25 18:15:36 +0530

    removed s3 buckets

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -10,15 +10,7 @@ bucket="devon-credentials006"
 force_destroy=true
 }
 
-resource "aws_s3_bucket" "devon-credentials007"{
-bucket="devon-credentials007"
-force_destroy=true
-}
 
 
-resource "aws_s3_bucket" "devon-credentials008"{
-bucket="devon-credentials008"
-force_destroy=true
-}
 
 
