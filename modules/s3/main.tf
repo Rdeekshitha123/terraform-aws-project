a4667e01c1b1eac4f7890793dde5fa284a4c5f1e
Deekshitha R <149041394+Rdeekshitha123@users.noreply.github.com>
2025-08-25 22:16:18 +0530

    change3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -10,6 +10,14 @@ bucket="devon-credentials006"
 force_destroy=true
 }
 
+resource "aws_s3_bucket" "devon-credentials007"{
+bucket="devon-credentials007"
 
+force_destroy=true
+}
 
+resource "aws_s3_bucket" "devon-credentials005"{
+bucket="devon-credentials005"
 
+force_destroy=true
+}
